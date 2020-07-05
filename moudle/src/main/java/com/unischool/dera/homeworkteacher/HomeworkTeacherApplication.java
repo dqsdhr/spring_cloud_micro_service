1432a0c1bf5d43cce16915b362ab81743f26ef0c
杜骞 <me@duqian.com>
2020-07-05 21:33:47 +0800

    init

---
+++ moudle/src/main/java/com/unischool/dera/homeworkteacher/HomeworkTeacherApplication.java
@@ -0,0 +1,13 @@
+package com.mark.microservicehomeworkteacher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkTeacherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeworkTeacherApplication.class, args);
+    }
+
+}
---
+++ moudle/src/test/java/com/unischool/dera/homeworkteacher/HomeworkTeacherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mark.microservicehomeworkteacher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkTeacherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
